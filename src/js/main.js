3ccbfe606441e4f9a1ba96cef97f6843ff227ef0
shun916 <118881278+code-polaris044@users.noreply.github.com>
2023-11-05 22:23:45 +0900

    修正

--- src/js/main.js
+++ src/js/main.js
@@ -14,22 +14,48 @@
 // });
 
 // クラス名が "btn" の要素を選択する例
-const btn = document.querySelector(".btn");
+// const btn = document.querySelector(".btn");
 
-btn.addEventListener("click", (e) => {
-  //どちらか
-  console.log(btn);
-  console.log(e.target);
-});
+// btn.addEventListener("click", (e) => {
+//   //どちらか
+//   // console.log(btn);
+//   btn.classList.toggle("active");
+//   console.log(e.target);
+// });
 
-const obj = {
-  id: 1,
-  // name: "Tanaka",
-  age: 20,
-};
+// const obj = {
+//   id: 1,
+//   // name: "Tanaka",
+//   age: 20,
+// };
 
 // if (obj.name) {
 //   console.log(obj.name);
 // }
 
-console.log(obj?.name);
+// console.log(obj?.name);
+
+// const input = document.querySelector(".input");
+
+// const submit = document.querySelector(".submit");
+
+// input.addEventListener("input", (e) => {
+//   let val = e.target.value;
+//   console.log(val);
+//   console.log(e);
+// });
+
+// submit.addEventListener("click", (e) => {
+//   // ディフォルトの自動動作をリセット
+//   e.preventDefault();
+//   console.log(e);
+// });
+
+// const container = document.querySelector(".container");
+// const attr = container.getAttribute("id");
+// console.log(attr);
+
+// container.setAttribute("class", "wrapper");
+
+// container.classList.add("iii");
+// container.classList.remove("iii");
