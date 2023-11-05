e972550638ad75eafe8e0a22eff5d531817d6421
shun916 <118881278+code-polaris044@users.noreply.github.com>
2023-11-06 07:57:52 +0900

    修正

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
+window.addEventListener("load", function () {
+  // ローディング画面の要素を取得
+  const loader = document.querySelector(".loader");
 
+  // ローディング画面を非表示にするためにクラスを削除
+  loader.classList.add("hide-loader");
+
+  // ローディング画面を非表示にするアニメーションの時間（ミリ秒）
+  const animationDuration = 1000; // 1秒
+
+  // アニメーションが終了したらローディング画面を完全に非表示にする
+  setTimeout(function () {
+    loader.style.display = "none";
+  }, animationDuration);
+});
